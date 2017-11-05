9844c4e5482a4d33eb0a2670726615a44576097d
farhad faramarzi <farhad.public@gmail.com>
2017-11-05 09:56:31 +0330

    add min and max value

--- findingHome.py
+++ findingHome.py
@@ -4,15 +4,30 @@ import redis
 import requests
 
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
-response  = requests.get("https://divar.ir/tehran/تهران/سهروردی-جنوبی/browse/املاک-مسکن/فروش-مسکونی-آپارتمان-خانه-زمین/?v09=1,380000000,420000000&place=8,296")
+min = 380000000
+max = 440000000
+response  = requests.get("https://divar.ir/tehran/تهران/سهروردی-جنوبی/browse/املاک-مسکن/فروش-مسکونی-آپارتمان-خانه-زمین/?v09=1,{},{}&place=8,296".format(min, max))
 
 soup = BeautifulSoup(response.content, "html.parser")
+
+def isExist(text):
+    item = r.get(text)
+    if item == None:
+        return False
+    return True
+
+print("--- new Item ---")
 
 for link in soup.find_all('a'):
     if link.get('class') != None and len(link.get('class')) > 0 :
         if link.get('class')[0] == 'post-card-link':
-            r.set(link.get_text(), link.get_text())
+            home = link.get_text()
+            if isExist(home) == False:
+                r.set(home, home)
+                print(home)
 
+
+print("--- old Item ---")
 keys = r.keys('*')
 for key in keys:
-    print(key)
+    print(r.get(key))
